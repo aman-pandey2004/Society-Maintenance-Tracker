166ade001c209e362cd3b216779fe79f50a07ec7
Aman Pandey <amanpandeyd24@gmail.com>
2026-07-03 19:45:12 +0530

    Updated admin credentials to 1234 without comments

--- society_tracker/urls.py
+++ society_tracker/urls.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.core.management import call_command
+from django.contrib.auth.models import User
+
+try:
+    call_command('migrate', interactive=False)
+    
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@society.com', '1234')
+        
+    if not User.objects.filter(username='resident').exists():
+        User.objects.create_user('resident', 'resident@society.com', '1234')
+except Exception as e:
+    pass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
